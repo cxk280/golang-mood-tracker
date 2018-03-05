2941663277d0e27c10522f4e81fa3f3d958f0d55
Christopher King <chris@cking.me>
2018-03-05 12:49:50 -0500

    update diary model with new form fields

--- forms/diary.go
+++ forms/diary.go
@@ -2,6 +2,6 @@ package forms
 
 //DiaryForm ...
 type DiaryForm struct {
-  Feeling   string `form:"feeling" json:"feeling" binding:"required,max=100"`
-  Notes     string `form:"notes" json:"notes" binding:"required,max=1000"`
+  Feeling   int64   `form:"feeling" json:"feeling" binding:"required,max=100"`
+  Notes     string  `form:"notes" json:"notes" binding:"required,max=1000"`
 }
--- models/diary.go
+++ models/diary.go
@@ -12,8 +12,8 @@ import (
 type Diary struct {
   ID        int64    `db:"id, primarykey, autoincrement" json:"id"`
   UserID    int64    `db:"user_id" json:"-"`
-  Title     string   `db:"title" json:"title"`
-  Content   string   `db:"content" json:"content"`
+  Feeling   int64    `db:"feeling" json:"feeling"`
+  Notes     string   `db:"notes" json:"notes"`
   UpdatedAt int64    `db:"updated_at" json:"updated_at"`
   CreatedAt int64    `db:"created_at" json:"created_at"`
   User      *JSONRaw `db:"user" json:"user"`
@@ -34,7 +34,7 @@ func (m DiaryModel) Create(userID int64, form forms.DiaryForm) (diaryID int64, e
     return 0, errors.New("User doesn't exist")
   }
 
-  _, err = getDb.Exec("INSERT INTO public.diary(user_id, title, content, updated_at, created_at) VALUES($1, $2, $3, $4, $5) RETURNING id", userID, form.Title, form.Content, time.Now().Unix(), time.Now().Unix())
+  _, err = getDb.Exec("INSERT INTO public.diary(user_id, feeling, notes, updated_at, created_at) VALUES($1, $2, $3, $4, $5) RETURNING id", userID, form.Feeling, form.Notes, time.Now().Unix(), time.Now().Unix())
 
   if err != nil {
     return 0, err
@@ -47,13 +47,13 @@ func (m DiaryModel) Create(userID int64, form forms.DiaryForm) (diaryID int64, e
 
 //One ...
 func (m DiaryModel) One(userID, id int64) (diary Diary, err error) {
-  err = db.GetDB().SelectOne(&diary, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.diary a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 AND a.id=$2 LIMIT 1", userID, id)
+  err = db.GetDB().SelectOne(&diary, "SELECT a.id, a.feeling, a.notes, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.diary a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 AND a.id=$2 LIMIT 1", userID, id)
   return diary, err
 }
 
 //All ...
 func (m DiaryModel) All(userID int64) (diaries []Diary, err error) {
-  _, err = db.GetDB().Select(&diaries, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.diary a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 ORDER BY a.id DESC", userID)
+  _, err = db.GetDB().Select(&diaries, "SELECT a.id, a.feeling, a.notes, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.diary a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 ORDER BY a.id DESC", userID)
   return diaries, err
 }
 
@@ -65,7 +65,7 @@ func (m DiaryModel) Update(userID int64, id int64, form forms.DiaryForm) (err er
     return errors.New("diary not found")
   }
 
-  _, err = db.GetDB().Exec("UPDATE public.diary SET title=$1, content=$2, updated_at=$3 WHERE id=$4", form.Title, form.Content, time.Now().Unix(), id)
+  _, err = db.GetDB().Exec("UPDATE public.diary SET feeling=$1, notes=$2, updated_at=$3 WHERE id=$4", form.Feeling, form.Notes, time.Now().Unix(), id)
 
   return err
 }
