bb1bfbf6b833d8e035ae63b89f026e5a0c2f40c7
Christopher King <chris@cking.me>
2018-03-04 19:41:38 -0500

    update feed controller

--- controllers/feed.go
+++ controllers/feed.go
@@ -15,139 +15,138 @@ type FeedController struct{}
 
 var feedModel = new(models.FeedModel)
 
-//Create ...
-// func (ctrl FeedController) Create(c *gin.Context) {
-//   userID := getUserID(c)
+Create ...
+func (ctrl FeedController) Create(c *gin.Context) {
+  userID := getUserID(c)
 
-//   if userID == 0 {
-//     c.JSON(403, gin.H{"message": "Please login first"})
-//     c.Abort()
-//     return
-//   }
+  if userID == 0 {
+    c.JSON(403, gin.H{"message": "Please login first"})
+    c.Abort()
+    return
+  }
 
-//   var feedForm forms.FeedForm
+  var feedForm forms.FeedForm
 
-//   if c.BindJSON(&feedForm) != nil {
-//     c.JSON(406, gin.H{"message": "Invalid form", "form": feedForm})
-//     c.Abort()
-//     return
-//   }
+  //Make sure to use lower-case keys in the form data
+  if err := c.ShouldBindWith(&feedForm, binding.Form); err != nil {
+    c.JSON(406, gin.H{"message": "Invalid form", "form": feedForm})
+    c.Abort()
+    return
+  }
 
-//   feedID, err := feedModel.Create(userID, feedForm)
+  feedID, err := feedModel.Create(userID, feedForm)
 
-//   if feedID > 0 && err != nil {
-//     c.JSON(406, gin.H{"message": "feed could not be created", "error": err.Error()})
-//     c.Abort()
-//     return
-//   }
+  if feedID > 0 && err != nil {
+    c.JSON(406, gin.H{"message": "feed could not be created", "error": err.Error()})
+    c.Abort()
+    return
+  }
 
-//   c.JSON(200, gin.H{"message": "feed created", "id": feedID})
-// }
+  c.JSON(200, gin.H{"message": "feed created", "id": feedID})
+}
 
 //All ...
 func (ctrl FeedController) All(c *gin.Context) {
-  // userID := getUserID(c)
+  userID := getUserID(c)
 
-  // if userID == 0 {
-  //   c.JSON(403, gin.H{"message": "Please login first"})
-  //   c.Abort()
-  //   return
-  // }
+  if userID == 0 {
+    c.JSON(403, gin.H{"message": "Please login first"})
+    c.Abort()
+    return
+  }
 
-  // data, err := feedModel.All(userID)
+  data, err := feedModel.All(userID)
 
-  // if err != nil {
-  //   c.JSON(406, gin.H{"Message": "Could not get the feeds", "error": err.Error()})
-  //   c.Abort()
-  //   return
-  // }
-
-  // c.JSON(200, gin.H{"data": data})
+  if err != nil {
+    c.JSON(406, gin.H{"Message": "Could not get the feeds", "error": err.Error()})
+    c.Abort()
+    return
+  }
 
   c.HTML(http.StatusOK, "analytics.html", gin.H{
     })
 }
 
-// //One ...
-// func (ctrl FeedController) One(c *gin.Context) {
-//   userID := getUserID(c)
+//One ...
+func (ctrl FeedController) One(c *gin.Context) {
+  userID := getUserID(c)
 
-//   if userID == 0 {
-//     c.JSON(403, gin.H{"message": "Please login first"})
-//     c.Abort()
-//     return
-//   }
+  if userID == 0 {
+    c.JSON(403, gin.H{"message": "Please login first"})
+    c.Abort()
+    return
+  }
 
-//   id := c.Param("id")
+  id := c.Param("id")
 
-//   if id, err := strconv.ParseInt(id, 10, 64); err == nil {
+  if id, err := strconv.ParseInt(id, 10, 64); err == nil {
 
-//     data, err := feedModel.One(userID, id)
-//     if err != nil {
-//       c.JSON(404, gin.H{"Message": "feed not found", "error": err.Error()})
-//       c.Abort()
-//       return
-//     }
-//     c.JSON(200, gin.H{"data": data})
-//   } else {
-//     c.JSON(404, gin.H{"Message": "Invalid parameter"})
-//   }
-// }
+    data, err := feedModel.One(userID, id)
+    if err != nil {
+      c.JSON(404, gin.H{"Message": "feed not found", "error": err.Error()})
+      c.Abort()
+      return
+    }
+    c.JSON(200, gin.H{"data": data})
+  } else {
+    c.JSON(404, gin.H{"Message": "Invalid parameter"})
+  }
+}
 
-// //Update ...
-// func (ctrl FeedController) Update(c *gin.Context) {
-//   userID := getUserID(c)
+//Update ...
+func (ctrl FeedController) Update(c *gin.Context) {
+  userID := getUserID(c)
 
-//   if userID == 0 {
-//     c.JSON(403, gin.H{"message": "Please login first"})
-//     c.Abort()
-//     return
-//   }
+  if userID == 0 {
+    c.JSON(403, gin.H{"message": "Please login first"})
+    c.Abort()
+    return
+  }
 
-//   id := c.Param("id")
-//   if id, err := strconv.ParseInt(id, 10, 64); err == nil {
+  id := c.Param("id")
+  if id, err := strconv.ParseInt(id, 10, 64); err == nil {
 
-//     var feedForm forms.FeedForm
+    var feedForm forms.FeedForm
 
-//     if c.BindJSON(&feedForm) != nil {
-//       c.JSON(406, gin.H{"message": "Invalid parameters", "form": feedForm})
-//       c.Abort()
-//       return
-//     }
+    if c.BindJSON(&feedForm) != nil {
+      c.JSON(406, gin.H{"message": "Invalid parameters", "form": feedForm})
+      c.Abort()
+      return
+    }
 
-//     err := feedModel.Update(userID, id, feedForm)
-//     if err != nil {
-//       c.JSON(406, gin.H{"Message": "feed could not be updated", "error": err.Error()})
-//       c.Abort()
-//       return
-//     }
-//     c.JSON(200, gin.H{"message": "feed updated"})
-//   } else {
-//     c.JSON(404, gin.H{"Message": "Invalid parameter", "error": err.Error()})
-//   }
-// }
+    err := feedModel.Update(userID, id, feedForm)
+    if err != nil {
+      c.JSON(406, gin.H{"Message": "feed could not be updated", "error": err.Error()})
+      c.Abort()
+      return
+    }
+    c.JSON(200, gin.H{"message": "feed updated"})
+  } else {
+    c.JSON(404, gin.H{"Message": "Invalid parameter", "error": err.Error()})
+  }
+}
 
-// //Delete ...
-// func (ctrl FeedController) Delete(c *gin.Context) {
-//   userID := getUserID(c)
+//Delete ...
+func (ctrl FeedController) Delete(c *gin.Context) {
+  userID := getUserID(c)
 
-//   if userID == 0 {
-//     c.JSON(403, gin.H{"message": "Please login first"})
-//     c.Abort()
-//     return
-//   }
+  if userID == 0 {
+    c.JSON(403, gin.H{"message": "Please login first"})
+    c.Abort()
+    return
+  }
 
-//   id := c.Param("id")
-//   if id, err := strconv.ParseInt(id, 10, 64); err == nil {
+  id := c.Param("id")
+  if id, err := strconv.ParseInt(id, 10, 64); err == nil {
 
-//     err := feedModel.Delete(userID, id)
-//     if err != nil {
-//       c.JSON(406, gin.H{"Message": "feed could not be deleted", "error": err.Error()})
-//       c.Abort()
-//       return
-//     }
-//     c.JSON(200, gin.H{"message": "feed deleted"})
-//   } else {
-//     c.JSON(404, gin.H{"Message": "Invalid parameter"})
-//   }
-// }
+    err := feedModel.Delete(userID, id)
+    if err != nil {
+      c.JSON(406, gin.H{"Message": "feed could not be deleted", "error": err.Error()})
+      c.Abort()
+      return
+    }
+    c.JSON(200, gin.H{"message": "feed deleted"})
+  } else {
+    c.JSON(404, gin.H{"Message": "Invalid parameter"})
+  }
+}
