6a56c9154f34e311f3a4e3b292ad545c17d0c958
Christopher King <chris@cking.me>
2018-03-11 12:49:26 -0400

    delete some console logs

--- public/scripts/jquery.js
+++ public/scripts/jquery.js
@@ -1,27 +1,20 @@
 $( document ).ready(function() {
-        console.log( "ready!" );
 
         $("#dashboard-header-text").hover(function () {
-          console.log('hovering');
           $(".dashboard-icon").attr("src","public/assets/dashboard_active.svg");
         }, function () {
-          console.log('no longer hovering');
           $(".dashboard-icon").attr("src","public/assets/dashboard_inactive.svg");
         });
 
         $("#diary-header-text").hover(function () {
-          console.log('hovering');
           $(".diary-icon").attr("src","public/assets/diary_active.svg");
         }, function () {
-          console.log('no longer hovering');
           $(".diary-icon").attr("src","public/assets/diary_inactive.svg");
         });
 
         $("#analytics-header-text").hover(function () {
-          console.log('hovering');
           $(".analytics-icon").attr("src","public/assets/analytics_active.svg");
         }, function () {
-          console.log('no longer hovering');
           $(".analytics-icon").attr("src","public/assets/analytics_inactive.svg");
         });
 
