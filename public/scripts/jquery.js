be7ad9d098b19077ac24d436119b7b4c90b167a6
Christopher King <chris@cking.me>
2018-03-11 16:28:23 -0400

    update jquery script to color icons corretly

--- public/scripts/jquery.js
+++ public/scripts/jquery.js
@@ -1,28 +1,116 @@
 $( document ).ready(function() {
 
-        $("#dashboard-header-text").hover(function () {
-          $(".dashboard-icon").attr("src","public/assets/dashboard_active.svg");
-        }, function () {
-          $(".dashboard-icon").attr("src","public/assets/dashboard_inactive.svg");
-        });
+        if (top.location.pathname === '/dashboard') {
+            $(".dashboard-icon").attr("src","public/assets/dashboard_active.svg");
+            $("#dashboard-header-text").css("color","#7FC347");
 
-        $("#diary-header-text").hover(function () {
-          $(".diary-icon").attr("src","public/assets/diary_active.svg");
-        }, function () {
-          $(".diary-icon").attr("src","public/assets/diary_inactive.svg");
-        });
+            $("#dashboard-header-text").hover(function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_hover.svg");
+            }, function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_active.svg");
+            });
 
-        $("#analytics-header-text").hover(function () {
-          $(".analytics-icon").attr("src","public/assets/analytics_active.svg");
-        }, function () {
-          $(".analytics-icon").attr("src","public/assets/analytics_inactive.svg");
-        });
+            $("#diary-header-text").hover(function () {
+              $(".diary-icon").attr("src","public/assets/diary_hover.svg");
+            }, function () {
+              $(".diary-icon").attr("src","public/assets/diary_inactive.svg");
+            });
 
-        // $("#logout-header-text").hover(function () {
-        //   console.log('hovering');
-        //   $(".logout-icon").attr("src","public/assets/logout_active.svg");
-        // }, function () {
-        //   console.log('no longer hovering');
-        //   $(".logout-icon").attr("src","public/assets/logout_inactive.svg");
-        // });
+            $("#analytics-header-text").hover(function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_hover.svg");
+            }, function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_inactive.svg");
+            });
+
+            $("#logout-header-text").hover(function () {
+              $(".logout-icon").attr("src","public/assets/logout_hover.svg");
+            }, function () {
+              $(".logout-icon").attr("src","public/assets/logout_inactive.svg");
+            });
+        }
+
+        if (top.location.pathname === '/diaryPage') {
+            $(".diary-icon").attr("src","public/assets/diary_active.svg");
+            $("#diary-header-text").css("color","#7FC347");
+
+            $("#dashboard-header-text").hover(function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_hover.svg");
+            }, function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_inactive.svg");
+            });
+
+            $("#diary-header-text").hover(function () {
+              $(".diary-icon").attr("src","public/assets/diary_hover.svg");
+            }, function () {
+              $(".diary-icon").attr("src","public/assets/diary_active.svg");
+            });
+
+            $("#analytics-header-text").hover(function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_hover.svg");
+            }, function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_inactive.svg");
+            });
+
+            $("#logout-header-text").hover(function () {
+              $(".logout-icon").attr("src","public/assets/logout_hover.svg");
+            }, function () {
+              $(".logout-icon").attr("src","public/assets/logout_inactive.svg");
+            });
+        }
+
+        if (top.location.pathname === '/analytics') {
+            $(".analytics-icon").attr("src","public/assets/analytics_active.svg");
+            $("#analytics-header-text").css("color","#7FC347");
+
+            $("#dashboard-header-text").hover(function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_hover.svg");
+            }, function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_inactive.svg");
+            });
+
+            $("#diary-header-text").hover(function () {
+              $(".diary-icon").attr("src","public/assets/diary_hover.svg");
+            }, function () {
+              $(".diary-icon").attr("src","public/assets/diary_inactive.svg");
+            });
+
+            $("#analytics-header-text").hover(function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_hover.svg");
+            }, function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_active.svg");
+            });
+
+            $("#logout-header-text").hover(function () {
+              $(".logout-icon").attr("src","public/assets/logout_hover.svg");
+            }, function () {
+              $(".logout-icon").attr("src","public/assets/logout_inactive.svg");
+            });
+        }
+
+        if ((top.location.pathname === '/') ||  (top.location.pathname === '/signup')) {
+            $("#dashboard-header-text").hover(function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_hover.svg");
+            }, function () {
+              $(".dashboard-icon").attr("src","public/assets/dashboard_inactive.svg");
+            });
+
+            $("#diary-header-text").hover(function () {
+              $(".diary-icon").attr("src","public/assets/diary_hover.svg");
+            }, function () {
+              $(".diary-icon").attr("src","public/assets/diary_inactive.svg");
+            });
+
+            $("#analytics-header-text").hover(function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_hover.svg");
+            }, function () {
+              $(".analytics-icon").attr("src","public/assets/analytics_inactive.svg");
+            });
+
+            $("#logout-header-text").hover(function () {
+              $(".logout-icon").attr("src","public/assets/logout_hover.svg");
+            }, function () {
+              $(".logout-icon").attr("src","public/assets/logout_inactive.svg");
+            });
+        }
+
 });
